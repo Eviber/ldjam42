a55da5b3398bf02736ddadc3a0146a69a3739c3c
Eclahn <lgdtheo@gmail.com>
2018-08-12 18:15:43 +0200

    Exit item created untested

--- items.lua
+++ items.lua
@@ -1,12 +1,24 @@
-Item = {x, y, val}
+Exit = {x, y, val, locked}
 
-function Item:new(x, y, val)
+function Exit:new(x, y, val, locked)
 	local o = {}
 	setmetatable(o, Item)
 	self.x = x
 	self.y = y
 	self.val = val
+	self.locked = locked
 	return o
 end
 
-Exit = Item.new(
+function Exit:changeLock(status)
+	self.locked = status
+end
+
+function Exit:goThrough()
+	player.health = player.health + 10 > 100 and 100 or player.health + 10
+	iter = Iteration:new(iter.id - 1, W / 4, H / 4, 1)
+end
+	
+--HealthPack = {x, y, val}
+
+--function HealthPack:new(x, y, val)
