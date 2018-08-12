c62be522155f54d21c9ffe663034ee7c6346cfd9
Eviber <youv.didj@gmail.com>
2018-08-12 13:40:24 +0200

    indent clean

--- iteration.lua
+++ iteration.lua
@@ -7,101 +7,101 @@ Tile = {x, y, state = 1}
 --Debug
 --[[
 function readTable (tab)
-  for a, b in pairs(tab) do
-    for c, d in pairs(tab[a]) do
-      print(tab[a][c].state .. " ")
-    end
-    print("\n")
-  end
+for a, b in pairs(tab) do
+for c, d in pairs(tab[a]) do
+print(tab[a][c].state .. " ")
+end
+print("\n")
+end
 end
 ]]
 --End debug
 
 
 function Tile:new (x, y, state)
-  local o = {}
-  setmetatable(o, Tile)
-  self.x = x
-  self.y = y
-  self.state = state or self.state
-  return o
+	local o = {}
+	setmetatable(o, Tile)
+	self.x = x
+	self.y = y
+	self.state = state or self.state
+	return o
 end
 
 function Field:new (width, height)
-  setmetatable({}, Field)
-  local i, j 
-  for i = 0, width do
-    self[i] = {}
-    for j = 0, height - 1 do
-      self[i][j] = Tile:new(i, j, 1)
-    end
-  end
-  return self
+	setmetatable({}, Field)
+	local i, j 
+	for i = 0, width do
+		self[i] = {}
+		for j = 0, height - 1 do
+			self[i][j] = Tile:new(i, j, 1)
+		end
+	end
+	return self
 end
 
 function Iteration:new (id, width, height, decay)
-  setmetatable({}, Iteration)
-  self.id = id
-  self.width = width or self.width
-  self.height = height or self.height
-  self.decay = decay or self.decay
-  self.field = Field:new(self.width, self.height)
-  return self
+	setmetatable({}, Iteration)
+	self.id = id
+	self.width = width or self.width
+	self.height = height or self.height
+	self.decay = decay or self.decay
+	self.field = Field:new(self.width, self.height)
+	return self
 end
 
 function removeCol (index, target)
-  for i = 0, target.height - 1 do
-    target.field[index][i].state = 0
-  end
+	for i = 0, target.height - 1 do
+		target.field[index][i].state = 0
+	end
 end
 
 function removeLine (index, target)
-  for i = 0, target.width - 1 do
-    target.field[i][index].state = 0
-  end
+	for i = 0, target.width - 1 do
+		target.field[i][index].state = 0
+	end
 end
-  
+
 function reduceField (currentIter)
-  if math.ceil(currentIter.width / 2) > currentIter.decay or math.ceil(currentIter.height / 2) > currentIter.decay then 
-    if math.ceil(currentIter.width / 2) > currentIter.decay then
-      removeCol(currentIter.decay - 1, currentIter)
-      removeCol(currentIter.width - currentIter.decay, currentIter)
-    end
-    if math.ceil(currentIter.height / 2) > currentIter.decay then
-      removeLine(currentIter.decay - 1, currentIter)
-      removeLine(currentIter.height - currentIter.decay, currentIter)
-    end
-    currentIter.decay = currentIter.decay + 1
-  else
-    local newIter = Iteration:new(currentIter.id - 1, 7, 5, 1)
-    return newIter
-  end
-  return currentIter
+	if math.ceil(currentIter.width / 2) > currentIter.decay or math.ceil(currentIter.height / 2) > currentIter.decay then 
+		if math.ceil(currentIter.width / 2) > currentIter.decay then
+			removeCol(currentIter.decay - 1, currentIter)
+			removeCol(currentIter.width - currentIter.decay, currentIter)
+		end
+		if math.ceil(currentIter.height / 2) > currentIter.decay then
+			removeLine(currentIter.decay - 1, currentIter)
+			removeLine(currentIter.height - currentIter.decay, currentIter)
+		end
+		currentIter.decay = currentIter.decay + 1
+	else
+		local newIter = Iteration:new(currentIter.id - 1, 7, 5, 1)
+		return newIter
+	end
+	return currentIter
 end
 
 function printTable (cible)
-  local i, j
-  for i = 0, iter.width - 1 do
-    for j = 0, iter.height - 1 do
-      io.write(iter.field[i][j].state.." ")
-    end
-    io.write("\n")
-  end
-  io.write("\n")
+	local i, j
+	for i = 0, iter.width - 1 do
+		for j = 0, iter.height - 1 do
+			io.write(iter.field[i][j].state.." ")
+		end
+		io.write("\n")
+	end
+	io.write("\n")
 end
 --[[print("hai")
 for k, j in pairs(iter) do
-  print(k, j)
+print(k, j)
 end]]
 
 --print(iter.field)
 --readTable(iter.field)
 
 --[[while iter.id >= -5 do
-  iter = reduceField(iter)
-  print("The current field is field n°" .. tostring(iter.id) .. " and its decay is " .. tostring(iter.decay))
-  if iter.id < -5 then
-    print("GAME OVER")
-  end
+iter = reduceField(iter)
+print("The current field is field n°" .. tostring(iter.id) .. " and its decay is " .. tostring(iter.decay))
+if iter.id < -5 then
+print("GAME OVER")
+end
 end]]
 
--- main.lua
+++ main.lua
@@ -12,27 +12,27 @@ function love.load()
 	W, H = lg.getWidth(), lg.getHeight()
 	w,h = 5,5
 	Player = {x = math.random(W-w), y = math.random(H-h), w = w, h = h}
-  iter = Iteration:new(0, W / 4, H / 4, 1)
-  tileDim = math.min(W / iter.width, H / iter.height)
-  timeSum = 0
+	iter = Iteration:new(0, W / 4, H / 4, 1)
+	tileDim = math.min(W / iter.width, H / iter.height)
+	timeSum = 0
 end
 
 function love.draw()
-  drawField(iter)
-  lg.setColor(0,255,0,255)
+	drawField(iter)
+	lg.setColor(0,255,0,255)
 	lg.line(Player.x, Player.y, dirx, diry)
 	lg.ellipse('fill', Player.x, Player.y, Player.w, Player.h)
 	--[[if collision then
-		lg.print('COLLISION!!!', W/2, 0)
+	lg.print('COLLISION!!!', W/2, 0)
 	end]]
 end
 
 function love.update(dt)
-  timeSum = timeSum + dt
-  if timeSum > 0.5 then
-    timeSum = timeSum - 0.5
-    iter = reduceField(iter)
-    --print("The current field is field n°" .. tostring(iter.id) .. " and its decay is " .. tostring(iter.decay))
-  end
+	timeSum = timeSum + dt
+	if timeSum > 0.5 then
+		timeSum = timeSum - 0.5
+		iter = reduceField(iter)
+		--print("The current field is field n°" .. tostring(iter.id) .. " and its decay is " .. tostring(iter.decay))
+	end
 	controls.getInput(dt)
 end
