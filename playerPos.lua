8e759387cd3b3901b5b03e56c627025f574e4f3b
Eviber <youv.didj@gmail.com>
2018-08-27 02:32:49 +0200

    set even bigger hitbox

--- playerPos.lua
+++ playerPos.lua
@@ -24,13 +24,13 @@ function posReact(dt)
 		immunityTimer = 0
 		Player.health = healthRegen(dt)
 	end
-	for i = -6, 6 do
-		for j = -6, 6 do
+	for i = -10, 10 do
+		for j = -10, 10 do
 			local x, y = tileX + i, tileY + j
 			if x >= 0 and y >= 0 and x < iter.width and y < iter.height then
 				local val = math.abs(iter.field[tileX + i][tileY + j])
 				local xx, yy = x * tileDim + tileDim / 2, y * tileDim + tileDim / 2
-				if math.sqrt((xx - Player.x)^2 + (yy - Player.y)^2) < 30 and val > 1 --[[and itemList[val].val ~= 4]] then
+				if math.sqrt((xx - Player.x)^2 + (yy - Player.y)^2) < 50 and val > 1 --[[and itemList[val].val ~= 4]] then
 					itemList[val].interact(itemList[val])
 				end
 			end
